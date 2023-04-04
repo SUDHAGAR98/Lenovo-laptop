b2a099c58434c5323cb18390cb7cd7df59eb14ab
SUDHAGAR98 <sudhagar20398@gmail.com>
2023-04-04 19:13:05 +0530

    training

---
+++ src/test/java/org/in/Employe.java
@@ -0,0 +1,19 @@
+package org.in;
+
+public class Employe {
+	        private void method1() {
+            System.out.println("1");
+	         }
+	        
+	        
+	        private void method2() {
+		    System.out.println("2");
+			 
+	        }
+	        private void method3() {
+				System.out.println("3");
+
+	        }
+	        
+	        
+}
